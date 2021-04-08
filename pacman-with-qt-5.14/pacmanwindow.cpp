2cd98f35d587b9b6e1ada9a41f457b5d195c6044
balico <etienne.guillerm@gmail.com>
2021-04-08 15:17:47 +0200

    Modif sur la fenetre princip
    rajout fonction et début menu()

--- pacman-with-qt-5.14/pacmanwindow.cpp
+++ pacman-with-qt-5.14/pacmanwindow.cpp
@@ -5,76 +5,100 @@ using namespace std;
 
 PacmanWindow::PacmanWindow(QWidget *pParent, Qt::WindowFlags flags):QFrame(pParent, flags)
 {
-    // Taille des cases en pixels
-    int largeurCase, hauteurCase;
+  verif_images();
 
-    if (pixmapPacmanJ1.load("./data/pacmanJ1.bmp")==false)
-    {
-        cout<<"Impossible d'ouvrir pacman.png"<<endl;
-        exit(-1);
-    }
+  // Init_menu();
 
-    if (pixmapPacmanJ2.load("./data/pacmanJ2.bmp")==false)
-    {
-        cout<<"Impossible d'ouvrir pacman.png"<<endl;
-        exit(-1);
-    }
-
-    if (pixmapFantome.load("./data/fantome.bmp")==false)
-    {
-        cout<<"Impossible d'ouvrir fantome.bmp"<<endl;
-        exit(-1);
-    }
-
-    if (pixmapMur.load("./data/mur.bmp")==false)
-    {
-        cout<<"Impossible d'ouvrir mur.bmp"<<endl;
-        exit(-1);
-    }
-
-    if (pixmapVitre.load("./data/gate.png")==false)
-    {
-        cout<<"Impossible d'ouvrir gate.png"<<endl;
-        exit(-1);
-    }
-
-    if (pixmapGomme.load("./data/dot.png")==false)
-    {
-        cout<<"Impossible d'ouvrir dot.png"<<endl;
-        exit(-1);
-    }
-
-    if (pixmapPower.load("./data/power_ball.png")==false)
-    {
-        cout<<"Impossible d'ouvrir power_ball.png"<<endl;
-        exit(-1);
-    }
-
-    // Bouton pour nb fantomes
-    PacmanButton *btnAddFantome = new PacmanButton(this);
-    btnAddFantome->setText("Ajouter fantome");
-    btnAddFantome->setGeometry(10,8,100,17);
-    connect(btnAddFantome, &QPushButton::clicked, this, &PacmanWindow::btnAjouterFantome);
-
-    PacmanButton *btnRmvFantome = new PacmanButton(this);
-    btnRmvFantome->setText("Enlever fantome");
-    btnRmvFantome->setGeometry(10+100+10,8,100,17);
-    connect(btnRmvFantome, &QPushButton::clicked, this, &PacmanWindow::btnSupprFantome);
-
-    jeu.init();
-
-    QTimer *timer = new QTimer(this);
-    connect(timer, &QTimer::timeout, this, &PacmanWindow::handleTimer);
-    timer->start(500); //Temps en ms entre chaque action des fantomes
-
-    largeurCase = pixmapMur.width();
-    hauteurCase = pixmapMur.height();
-
-    resize(jeu.getNbCasesX()*largeurCase, jeu.getNbCasesY()*hauteurCase);
+  lancement_jeu();
 }
 
-void PacmanWindow::paintEvent(QPaintEvent *)
-{
+void PacmanWindow::lancement_jeu(){
+  // Taille des cases en pixels
+  int largeurCase, hauteurCase;
+  // On check si toutes les images sont ok
+
+  // Bouton pour nb fantomes
+  btnAddFantome = new PacmanButton(this);
+  btnAddFantome->setText("Ajouter fantome");
+  btnAddFantome->setGeometry(10,8,100,17);
+  connect(btnAddFantome, &QPushButton::clicked, this, &PacmanWindow::btnAjouterFantome);
+
+  btnRmvFantome = new PacmanButton(this);
+  btnRmvFantome->setText("Enlever fantome");
+  btnRmvFantome->setGeometry(10+100+10,8,100,17);
+  connect(btnRmvFantome, &QPushButton::clicked, this, &PacmanWindow::btnSupprFantome);
+
+  jeu.init();
+
+  QTimer *timer = new QTimer(this);
+  connect(timer, &QTimer::timeout, this, &PacmanWindow::handleTimer);
+  timer->start(500); //Temps en ms entre chaque action des fantomes
+
+  largeurCase = pixmapMur.width();
+  hauteurCase = pixmapMur.height();
+
+  resize(jeu.getNbCasesX()*largeurCase, jeu.getNbCasesY()*hauteurCase);
+}
+
+void PacmanWindow::Init_menu(){
+
+  // Bouton pour nb fantomes
+  btnMulti = new PacmanButton(this);
+  btnMulti->setText("Multijoueur (2)");
+  btnMulti->setGeometry(10,8,100,17);
+  connect(btnMulti, &QPushButton::clicked, this, &PacmanWindow::btnGestionMulti);
+
+}
+
+void PacmanWindow::Init_bouton_jeu(){
+  ;
+}
+
+void PacmanWindow::verif_images(){
+  if (pixmapPacmanJ1.load("./data/pacmanJ1.bmp")==false)
+  {
+      cout<<"Impossible d'ouvrir pacman.png"<<endl;
+      exit(-1);
+  }
+
+  if (pixmapPacmanJ2.load("./data/pacmanJ2.bmp")==false)
+  {
+      cout<<"Impossible d'ouvrir pacman.png"<<endl;
+      exit(-1);
+  }
+
+  if (pixmapFantome.load("./data/fantome.bmp")==false)
+  {
+      cout<<"Impossible d'ouvrir fantome.bmp"<<endl;
+      exit(-1);
+  }
+
+  if (pixmapMur.load("./data/mur.bmp")==false)
+  {
+      cout<<"Impossible d'ouvrir mur.bmp"<<endl;
+      exit(-1);
+  }
+
+  if (pixmapVitre.load("./data/gate.png")==false)
+  {
+      cout<<"Impossible d'ouvrir gate.png"<<endl;
+      exit(-1);
+  }
+
+  if (pixmapGomme.load("./data/dot.png")==false)
+  {
+      cout<<"Impossible d'ouvrir dot.png"<<endl;
+      exit(-1);
+  }
+
+  if (pixmapPower.load("./data/power_ball.png")==false)
+  {
+      cout<<"Impossible d'ouvrir power_ball.png"<<endl;
+      exit(-1);
+  }
+}
+
+void PacmanWindow::paintEvent(QPaintEvent *){
     QPainter painter(this);
     list<Fantome>::const_iterator itFantome;
     int x, y;
@@ -107,8 +131,7 @@ void PacmanWindow::paintEvent(QPaintEvent *)
   painter.drawPixmap(jeu.pacmanJ2.getPacmanX()*largeurCase, jeu.pacmanJ2.getPacmanY()*hauteurCase, pixmapPacmanJ2);
 }
 
-void PacmanWindow::keyPressEvent(QKeyEvent *event)
-{
+void PacmanWindow::keyPressEvent(QKeyEvent *event){
     if (event->key()==Qt::Key_Left)
         jeu.deplacePacman(GAUCHE, jeu.pacmanJ1);
     else if (event->key()==Qt::Key_Right)
@@ -129,18 +152,19 @@ void PacmanWindow::keyPressEvent(QKeyEvent *event)
     update();
 }
 
-void PacmanWindow::btnAjouterFantome()
-{
+void PacmanWindow::btnAjouterFantome(){
     jeu.AjouterFantome();
 }
 
-void PacmanWindow::btnSupprFantome()
-{
+void PacmanWindow::btnGestionMulti(){
+    std::cout << "Hello" << '\n';
+}
+
+void PacmanWindow::btnSupprFantome(){
     jeu.SupprFantome();
 }
 
-void PacmanWindow::handleTimer()
-{
+void PacmanWindow::handleTimer(){
     jeu.evolue();
     update();
 }
@@ -149,8 +173,7 @@ PacmanButton::PacmanButton(QWidget *p):QPushButton(p){
   ;
 }
 
-void PacmanButton::keyPressEvent(QKeyEvent *event)
-{
+void PacmanButton::keyPressEvent(QKeyEvent *event){
     if (parent()!=NULL){
       QCoreApplication::sendEvent(parent(),event);
     }
--- pacman-with-qt-5.14/pacmanwindow.h
+++ pacman-with-qt-5.14/pacmanwindow.h
@@ -5,29 +5,46 @@
 #include <QtWidgets/QtWidgets>
 #include "jeu.h"
 
+class PacmanButton;
+
 class PacmanWindow : public QFrame
 {
   protected:
     Jeu jeu;
-    QPixmap pixmapPacmanJ1, pixmapPacmanJ2, pixmapFantome, pixmapMur, pixmapGomme, pixmapVitre, pixmapPower;
+    QPixmap pixmapPacmanJ1, pixmapPacmanJ2, pixmapFantome,
+      pixmapMur, pixmapGomme, pixmapVitre, pixmapPower;
+
+    QBoxLayout *layout_principal;
+    QMainWindow *main_window;
+
+    PacmanButton *btnAddFantome, *btnRmvFantome;
+    PacmanButton *btnMulti;
 
   public:
     PacmanWindow(QWidget *pParent=0, Qt::WindowFlags flags=0);
     void handleTimer();
 
   protected:
+    void verif_images();
+
+    void Init_menu();
+    //initialisation des boutons present pendant le jeu (pause, play, ajout d'ennemi...)
+    void Init_bouton_jeu();
+    //lance le jeu apres un appui sur play dans le menu principal
+    void lancement_jeu();
+
     void paintEvent(QPaintEvent *);
     void keyPressEvent(QKeyEvent *);
+
     void btnAjouterFantome();
     void btnSupprFantome();
+    void btnGestionMulti();
 };
 
 class PacmanButton : public QPushButton
 {
     public:
       PacmanButton(QWidget *);
-
-    protected:
       void keyPressEvent(QKeyEvent *);
 };
 
